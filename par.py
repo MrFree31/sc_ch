fca671f76cd8d3d16706536e45bc5f66b3850a86
MrFree31 <kon.eremeev@gmail.com>
2026-03-29 18:29:12 +1000

    29.03.2026 Настройка сканирования и перекладывания в конкретную папку
    Настройка программы @askolenko_1 для вывода файлов из картинки в текст с самим конвертированием справилось, но сканы посредственного качества. Нужно с настройками самого Tesseract разбираться.

--- par.py
+++ par.py
@@ -1,7 +1,11 @@
 import json, pandas, subprocess #Добавляем библиотеки для json, работы с excel, файлом С
-
-out_c = subprocess.check_output(["par.exe","receipt2.txt"],text=True)
-
+from utils import img_get_main as IGM# Программа для обработки изображений от @askolenko_1
+from utils.img_get_main import convert_folder
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"D:\Programm Files\Tesseract-OCR\tesseract.exe"
+convert_folder(r"C:\Users\koner\Documents\GitHub\sc_ch\checks",r"C:\Users\koner\Documents\GitHub\sc_ch\scans")
+# r"..." - "сырая" строка от ошибок с косыми палками
+out_c = subprocess.check_output(["par.exe",r"C:\Users\koner\Documents\GitHub\sc_ch\scans\Receipt0.txt"],text=True,encoding='utf-8')# Кодировка полетела, фиксируем какую надо
 data = json.loads(out_c)
 
 print(data["date"])
